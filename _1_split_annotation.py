68b8dea107457f612e09ae7089d076337efcddf6
lzq <14318391116@qq.com>
2024-11-16 00:22:28 +0800

    add dir adding

--- _1_split_annotation.py
+++ _1_split_annotation.py
@@ -1,5 +1,6 @@
 import json 
 import argparse
+import os
 parser = argparse.ArgumentParser(description="shift annotations.")
 parser.add_argument('--all_thread', type=int, default=1, help='the number of all thread runingg parallel')
 args = parser.parse_args()
@@ -10,6 +11,10 @@ with open(path_to_json, 'r') as f:
 
 annotation = data["annotations"]
 anno_dir_path="anno/"
+
+if os.path.exists(anno_dir_path) == False:
+    os.makedirs(anno_dir_path)
+
 for i in range(args.all_thread):
     with open(anno_dir_path+"fixations_train2014_anno_{}.json".format(i),"w") as f:
         json.dump(annotation[i::args.all_thread],f)
