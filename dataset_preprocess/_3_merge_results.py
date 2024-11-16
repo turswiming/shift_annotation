a21fa81fa8c6c44f0acb456f07001cc026d3c57a
lzq <14318391116@qq.com>
2024-11-16 21:18:04 +0800

    add val dataset process

--- dataset_preprocess/_3_merge_results.py
+++ dataset_preprocess/_3_merge_results.py
@@ -2,17 +2,30 @@ import json
 import os
 import numpy
 from tqdm import tqdm
-path_to_anno_dir = "anno/"
-path_to_fixations = "fixations_train2014.json"
-path_to_new_fixations = "fixations_train2014_shifted.json"
+import argparse
+parser = argparse.ArgumentParser(description="shift annotations.")
+parser.add_argument('--dataset_type', type=str, default="train", help='train or val')
+args = parser.parse_args()
 
+mode = "train"
+if mode == "train":
+    path_to_anno_dir = "anno/"
+    path_to_fixations = "fixations_train2014.json"
+    path_to_new_fixations = "fixations_train2014_shifted.json"
+    path_to_shifted_prefix = "fixations_train2014_shift_"
+elif mode == "val":
+    path_to_anno_dir = "anno_val/"
+    path_to_fixations = "fixations_val2014.json"
+    path_to_new_fixations = "fixations_val2014_shifted.json"
+    path_to_shifted_prefix = "fixations_val2014_shift_"
+    
 with open(path_to_fixations, 'r') as f:
     data = json.load(f)
 
 data["annotations"]=[]
 
 for shifted_path in os.listdir(path_to_anno_dir):
-    if shifted_path.startswith("fixations_train2014_shift_"):
+    if shifted_path.startswith(path_to_shifted_prefix):
         path = path_to_anno_dir+shifted_path
         with open(path,"r") as f:
             annos = json.load(f)
--- multithread.bash
+++ multithread.bash
@@ -2,10 +2,13 @@
 
 num_cores=$(nproc)
 num_cores=8
-python3 split_annotation.py --all_thread "$num_cores"
+dataset_type="val"
+python3 dataset_preprocess/_1_split_annotation.py --all_thread "$num_cores" --dataset_type "$dataset_type"
 for ((i=0; i<num_cores; i++)); do
     echo "Starting thread $i"
-    python3 readjson.py --all_thread "$num_cores" --thread_id "$i" &
+    python3 dataset_preprocess/_2_readjson.py --all_thread "$num_cores" --thread_id "$i" --dataset_type "$dataset_type" &
 done
 
 wait
+
+python3 dataset_preprocess/_3_merge_annotation.py --dataset_type "$dataset_type"
